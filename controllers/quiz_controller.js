418b38ca8c7274d6d440f5be72e653957b0ce2e9
Daniel Estremera <estu_dani@hotmail.com>
2015-07-23 13:03:28 +0200

    retoques autor/creditos 2

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -12,9 +12,7 @@ exports.answer = function(req, res){
 		res.render('quizes/answer' , {respuesta: "Incorrecto"});
 	}
 };
-<<<<<<< HEAD
 
-=======
 exports.author = function(req , res){
 	res.render('quizes/author' , {nombre: "Daniel" , apellidos: "Estremera Lopez", nacionalidad: "española" , nacimiento: "1990" })
 };
